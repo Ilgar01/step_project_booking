4d2d3cbc6ee8c2239ba8f44504452b7c8d082882
Ilgar01 <haciyevilqar0101@gmail.com>
2022-02-10 15:34:19 +0400

    Entity Person edited

--- src/main/java/az/iktlab/entity/Person.java
+++ src/main/java/az/iktlab/entity/Person.java
@@ -1,4 +1,32 @@
 package az.iktlab.entity;
 
 public class Person {
+    private int personId;
+    private String personName;
+    private String personSurname;
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonSurname() {
+        return personSurname;
+    }
+
+    public void setPersonSurname(String personSurname) {
+        this.personSurname = personSurname;
+    }
+
+
 }
